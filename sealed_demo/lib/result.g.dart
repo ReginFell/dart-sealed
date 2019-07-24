d5ff607eae0d454c3dc54f3684b6bfb4050074f3
Serhii Zabelnikov <ReginFell@gmail.com>
2019-07-24 20:41:30 +0300

    Add license Add little readme

--- sealed_demo/lib/result.g.dart
+++ sealed_demo/lib/result.g.dart
@@ -6,6 +6,22 @@ part of 'result.dart';
 // SealedClassGenerator
 // **************************************************************************
 
-class ResultSealed {}
-
-class Result2Sealed {}
+class ResultSealed {
+  R when<R>(
+    R Function(Success) success,
+    R Function(Failure) failure,
+    R Function(Idle) idle,
+  ) {
+    if (this is Success) {
+      return success(this as Success);
+    }
+    if (this is Failure) {
+      return failure(this as Failure);
+    }
+    if (this is Idle) {
+      return idle(this as Idle);
+    }
+    throw new Exception(
+        'If you got here, probably you forgot to regenerate the classes? Try running flutter packages pub run build_runner build');
+  }
+}
